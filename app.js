53180cf62a1a79a866ed1f086a46a2ce07cf5dca
KUCA Brice <kuca.b@sfeir.com>
2024-10-08 15:34:17 +0200

    feat: add swagger

--- app.js
+++ app.js
@@ -2,10 +2,26 @@ require('dotenv').config();
 const express = require('express');
 const apiRouter = require('./routes');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express')
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: "Book API",
+            version: "1.0.0"
+        },
+    },
+    apis: ['./routes/*.js']
+}
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions)
 
 mongoose
     .connect(process.env.DATABASE_URL)
@@ -15,6 +31,8 @@ mongoose
     .catch((e) => {
         console.log(e);
     });
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
 app.use('/api', apiRouter);
 
 app.listen(3000, () => {
