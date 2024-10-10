3e69d81c82421c5dbb3b7aca67ed4edd20743db4
KUCA Brice <kuca.b@sfeir.com>
2024-10-10 14:41:44 +0200

    update swager

--- routes/auth.js
+++ routes/auth.js
@@ -37,6 +37,34 @@ const authRouter = Router();
  *                   type: string
  */
 authRouter.post('/register', authController.register);
+
+/**
+ * @swagger
+ * /api/auth/login:
+ *   post:
+ *     summary: register create nex user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Book'
+ *     responses:
+ *       200:
+ *         description: The user created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: string
+ *                 email:
+ *                   type: string
+ *                 password:
+ *                   type: string
+ */
 authRouter.post('/login', authController.login);
 
 module.exports = authRouter;
